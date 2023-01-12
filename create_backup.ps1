358f78c9ec4a696320e0b8fd76dc17a647b6706d
volkanu <volkan-ucar@outlook.com>
2023-01-12 22:45:55 +0100

    Create create_backup.ps1

---
+++ create_backup.ps1
@@ -0,0 +1,34 @@
+<#
+.SYNOPSIS
+   Create Backup
+.DESCRIPTION
+    This PowerShell script creates backup to a folder. Scripts create FULL - INCREMENTAL - DIFFERENTIAL backup of a shared folder to a defined backup destination.
+.EXAMPLE
+    PS> ./create_backup
+
+.LINK
+    https://github.com/volkanu/powershell
+.NOTES
+    Author: Volkan U | License: CC0
+#>
+
+# Define the shared folder to be backed up
+$folder = "\\server\sharedfolder"
+
+# Define the backup destination
+$backup = "C:\backup"
+
+# Create a full backup of the shared folder
+$date = (Get-Date).ToString("yyyyMMdd")
+$dest = "$backup\full_$date"
+Robocopy $folder $dest /MIR /R:1 /W:1
+
+# Create an incremental backup of the shared folder
+$prevbackup = "$backup\full_$date"
+$dest = "$backup\inc_$date"
+Robocopy $folder $dest /MIR /R:1 /W:1 /XJ /XF *.bak /XD $prevbackup
+
+# Create a differential backup of the shared folder
+$prevbackup = "$backup\full_$date"
+$dest = "$backup\diff_$date"
+Robocopy $folder $dest /MIR /R:1 /W:1 /XJ /XF *.bak /XD $prevbackup
